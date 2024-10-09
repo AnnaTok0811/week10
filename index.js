b3a3d1372c887fa653528f8fb3fcd2aaa6b593bb
AnnaTok0811 <nekrasova-a-s@mail.ru>
2024-10-09 12:28:25 +0300

    галерея с 3мя фото

---
+++ index.js
@@ -0,0 +1,10 @@
+//alert(`Я учу JAVA SCRIPT!`);
+
+function leftArrow() {
+  const elem = document.getElementById("pic");
+  elem.src = "assets/images/pic2.jpg";
+}
+function rightArrow() {
+  const elem = document.getElementById("pic");
+  elem.src = "assets/images/pic3.jpg";
+}
